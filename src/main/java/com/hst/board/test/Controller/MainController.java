a29d797e27a93f7156e296b44a59a9a05c201c03
홍승택 <tmdxor66@naver.com>
2021-03-12 00:01:23 +0900

    자동완성 / 들여쓰기 설정 / GET, POST Http 통신 테스트

--- src/main/java/com/hst/board/test/Controller/MainController.java
+++ src/main/java/com/hst/board/test/Controller/MainController.java
@@ -1,7 +1,14 @@
 package com.hst.board.test.Controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 
 @RestController
@@ -11,5 +18,15 @@ public class MainController {
     public String Main() {
         return "ㅎㅇ";
     }
+
+    @PostMapping(value="path")
+    @ResponseBody
+    public Map<String, Object> postMethodName() {
+        Map<String, Object> res = new HashMap<String, Object>();
+        res.put("id", 1);
+        res.put("name", "홍승택");
+        return res;
+    }
+    
     
 }
